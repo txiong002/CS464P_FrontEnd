d7efbfb3ad404368b35c33ec47c8e89ff2642341
txiong002 <tfx22@yahoo.com>
2023-10-02 22:03:04 -0700

    HW01-TX: Completed algorithm to determined if number is a palindrome

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,38 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("#userInput");
+const result = document.querySelector("#result");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
+
+function handleInput(event) {
+    const inputValue = event.target.value;
+
+    const isPalindrome = checkIfPalindrome(inputValue);
+
+    if (isPalindrome) {
+        result.textContent = `${inputValue} is a palindrome`;
+    } else {
+        result.textContent = `${inputValue} is not a palindrome`;
+    }
+}
+
+function checkIfPalindrome(number) {
+    // Check to make sure number is positive
+    if (number < 0) {
+        return false;
+    }
+
+    // Put the number into an array for comparison
+    const digits = Array.from(String(number));
+
+    let left = 0;
+    let right = digits.length - 1;
+
+    while (left < right) {
+        if (digits[left] !== digits[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
