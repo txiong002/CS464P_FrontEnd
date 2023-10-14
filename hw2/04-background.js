4e9241e8385d82a4ada08442ce73cfce91262a3e
Tou Xiong <toufxiong83@gmail.com>
2023-10-14 02:14:12 -0700

    HW02-TX: completed excercise 04.  Implemented single button to allow it to start/stop.  Generated random colors and have it display in the background.

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,34 @@
-// Add your code here
+const button = document.querySelector("#mainButton");
+let interval;
+let isRunning = false;
+
+// Generate random colors
+function generateRandomColors() {
+    const dimAlpha = 0.5; // enter a value between 0 and 1 for brightness
+    const randomColors = `rgba(
+        ${Math.floor(Math.random() * 256)},
+        ${Math.floor(Math.random() * 256)},
+        ${Math.floor(Math.random() * 256)},
+        ${dimAlpha})`;
+    return randomColors;
+}
+
+// Update button status
+function updateButtonStatus() {
+    const buttonStatus = document.querySelector("#mainButton");
+    if (isRunning) {
+        clearInterval(interval);
+        buttonStatus.value = "Start";
+        buttonStatus.classList.remove("btn-danger");
+        buttonStatus.classList.add("btn-primary");
+    } else {
+        interval = setInterval(() => {
+            document.body.style.backgroundColor = generateRandomColors();
+        }, parseInt(document.querySelector("#userInput").value) * 1000);
+        buttonStatus.value = "Stop";
+        buttonStatus.classList.add("btn-danger");
+    }
+    isRunning = !isRunning;
+}
+
+button.addEventListener("click", updateButtonStatus);
